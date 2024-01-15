b58f936e53861ae491b9eebb91079b4e28c45618
tesloach ker <127243762+tesloachkerchan@users.noreply.github.com>
2024-01-15 08:43:19 +0300

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,40 @@
+import './topbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+export default function Topbar() {
+  return (
+    <div className='topbarContainer'>
+      <div className="topbarLeft">
+        <span className='logo'>TesSocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <SearchIcon />
+          <input placeholder='search for posts or friends' className="searchInput" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Home Page</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
